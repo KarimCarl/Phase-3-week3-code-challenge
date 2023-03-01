e810db97cd10763db9bd4252213e53aeba9de43f
KarimCarl <cndungu109@gmail.com>
2023-03-01 19:36:10 +0300

    Modified user.id to user

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ActiveRecord::Base
     has_many :users, through: :reviews
 
     def leave_review(user, star_rating, comment)
-        Review.create(user_id: user.id, star_rating: star_rating, comment: comment)
+        Review.create(user_id: user, star_rating: star_rating, comment: comment)
     end
     def print_all_reviews
         puts self.reviews.map{|review|"Review for #{review.product.name} by #{review.user.name}: #{review.star_rating}. #{review.comment}"}
