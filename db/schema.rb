a4fcf807b2889a28930fd34da8775a4ed04c93dd
KarimCarl <cndungu109@gmail.com>
2023-03-01 19:35:26 +0300

    File created by system

--- db/schema.rb
+++ db/schema.rb
@@ -5,26 +5,27 @@
 # This file is the source Rails uses to define your schema when running `bin/rails
 # db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those migrations use external dependencies or application code.
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
-# We strongly recommended that you check this file into your version control system.
+ActiveRecord::Schema.define(version: 2023_03_01_160845) do
 
-ActiveRecord::Schema.define(version: 2023_02_25_174640) do
-
-    create_table "products", force: :cascade do |t|
-      t.string "name"
-      t.integer "price"
-    end
-  
-    create_table "reviews", force: :cascade do |t|
-      t.string "comment"
-      t.integer "star_rating"
-      t.integer "product_id"
-      t.integer "user_id"
-    end
-  
-    create_table "users", force: :cascade do |t|
-      t.string "name"
-    end
-  
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
   end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "comment"
+    t.integer "star_rating"
+    t.integer "product_id"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+  end
+
+end
