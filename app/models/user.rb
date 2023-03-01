67574b387e2668e37fc3900ae291b10fc5366493
KarimCarl <cndungu109@gmail.com>
2023-03-01 20:17:03 +0300

    Added comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,3 +9,17 @@ class User < ActiveRecord::Base
         self.reviews.where(product: product).destroy_all
     end
 end
+
+
+#To run object association methods,
+#You first have to access the product or user or review
+#by creating a variable e.g 
+# users=User.first => variable users stores the first user
+
+#You can then call methods like user.reviews to view the user's reviews
+#users.product - product they reviewd
+#users.review - what users reviewed
+
+#Aggregate methods
+#users.leave_reviews("Amazing") - creates new review
+#users.print_review  - prints user review
